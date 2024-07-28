f81e6316e216f55ab87036979efeb6a6ec147e1f
Albin Xavier Varghese <154511248+albinxavier1@users.noreply.github.com>
2024-07-29 04:26:06 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from phi.assistant import Assistant
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.llm.groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+# Initialize the LLM and Assistant
+llm = Groq(model="llama3-70b-8192", api_key=os.getenv("api_key"))
+assistant = Assistant(llm=llm, tools=[DuckDuckGo()], show_tool_calls=False)
+
+# Set up the Streamlit app
+st.title("QBot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for chunk in assistant.run(prompt, stream=False):
+            full_response += chunk
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
